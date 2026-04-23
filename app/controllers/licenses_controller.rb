39cfad99849ccb242b6bd2103d49b3d6c5f56910
Thabo Mashaba <thabo1498@gmail.com>
2026-04-24 01:55:20 +0200

    pushing small updates straight to master since this is a playground

--- app/controllers/licenses_controller.rb
+++ app/controllers/licenses_controller.rb
@@ -1,22 +1,25 @@
 class LicensesController < ApplicationController
   protect_from_forgery with: :null_session
-
+  
   def index
     @licenses = License.all.order(created_at: :desc)
+    today = Date.today
+    soon = today + 7
+    @total = License.count
+    @expired = License.where("expiry_date < ?", today).count
+    @expiring_soon = License.where("expiry_date >= ? AND expiry_date <= ?", today, soon).count
+    @active = License.where("expiry_date >= ?", today).count  # includes expiring soon
   end
 
   def new
    @license = License.new
   end
-
   def send_expiry_emails
     CheckLicenseExpiryJob.perform_now
     redirect_to licenses_path, notice: "Expiry emails sent successfully"
   end
-
   def create
     license = License.new(license_params)
-
     if license.save
       redirect_to licenses_path, notice: "License created successfully"
     else
